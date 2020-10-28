9ecf28ae85e7a860efaa4eeee28f2e95247a8a04
Halke <dinohalilovichalke@gmail.com>
2020-10-28 17:22:07 +0100

    Added socket.io event handlers

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,19 @@
 const socket = io();
 
 socket.on("message", message => {
-    console.log(message);
+    // Message that is emitted only to the user that
+    // connected to app
+    socket.emit("message", "Welcome to ChatCord!");
 
-    // Broadcast when a user connects
+    // Broadcast when a user connects. That means the message is
+    // sent to everyone on chat except the user that is connecting.
+    socket.broadcast.emit("message", "A user has joined chat!");
+
+    // Sends the message to everyone including the user that is
+    // connecting to app.
+    // ---- io.emit("message", "")
+
+    socket.on("disconnect", () => {
+        io.emit("message", "A user has left the chat!");
+    });
 });
